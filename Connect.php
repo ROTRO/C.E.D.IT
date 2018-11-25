d304ba0b06581287c0671797e0ab8c774e48cb88
ROTRO <ROTRO@users.noreply.github.com>
2018-11-25 21:33:54 +0100

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,7 +1,7 @@
 <?php
 try {
-$bdd= new PDO("mysql:host=localhost; dbname=Questions", 'root', '');
-
+$bdd= new PDO("mysql:host=localhost; dbname=127_0_0_1", 'root', '');
+echo "Connection success: ";
 }
 catch(PDOException $e)
     {
