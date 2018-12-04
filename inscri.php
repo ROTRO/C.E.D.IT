f18f982460eb49c6389f5c6c82da295ef17a3dc5
ROTRO <ROTRO@users.noreply.github.com>
2018-12-04 23:28:09 +0100

    Update inscri.php

--- inscri.php
+++ inscri.php
@@ -29,15 +29,16 @@ catch(PDOException $e)
 
 
 
-      $sql =("INSERT INTO `student`(`cin`,`num_s`, `fname_s`, `lname_s`, `email`, `tel_s`, `login_s`, `pw_s`) VALUES ($cin,'$sn','$nom','$prenom','$email','$tel','$login','$password')");
+      $sql =("INSERT INTO `student`(`cin`,`num_s`, `fname_s`, `lname_s`, `email`, `tel_s`, `login_s`, `pw_s`,`file_name`) VALUES ($cin,'$sn','$nom','$prenom','$email','$tel','$login','$password','')");
+      echo $sql;
         $bdd->exec($sql);
         var_dump($bdd->exec($sql));
         echo "New record created successfully !";
         session_start();
         $_SESSION['nom']=$nom;
         $_SESSION['prenom']=$prenom;
-
-        header('location:student.php');
+        $_SESSION['cin']=$cin;
+    header('location:student.php');
 
         }
         catch(PDOException $e)
